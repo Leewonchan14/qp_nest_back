a77981d596fc46938154126bb4cbce9731dbe0a9
이원찬 <twoone14@tukorea.ac.kr>
2024-10-17 13:55:43 +0900

    global prefix로 api 설정 및 cors 설정

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   // class-validator가 NestJS의 의존성 주입 컨테이너를 사용하도록 설정
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
